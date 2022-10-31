3bbb1022a0070c001079464a450f676947624c18
大彭Sir <hk@hkiii.cn>
2022-10-31 22:47:49 +0800

    删除node依赖

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link>｜
     </nav> -->
     <router-view/>
   </div>
